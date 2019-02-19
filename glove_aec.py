a3135bf6062d75e3e0781508d4dcbebdb0b780f6
Michael Lomnitz <mllomnitz@gmail.com>
2019-02-19 21:02:31 +0000

    Renames save example as reun becnhmark. Adds subroutines to encode and decode messages seperately

--- glove_aec.py
+++ glove_aec.py
@@ -1,6 +1,9 @@
 # Standard python packages
+import os
 import re
+import shutil
 from random import sample
+import numpy as np
 # PyTorch imports
 import torch
 import torch.nn as nn
@@ -76,31 +79,9 @@ class encoder_decoder():
                   for pos in range(len(lst) + len(seq))]
 
     #
-    def save_example(self, message='', n_images=0, save_path='./Example'):
-        reco_message = ''
-        converted = self.convert_message(message=message,
-                                         n_images=n_images)
-        row = 0
-        for idx, cover in enumerate(self.coverloader):
-            if idx == len(converted)//self.n_words:
-                break
-            lower = idx*self.n_words
-            upper = (idx+1)*self.n_words
-            original, message, covered = self.hide_message(cover, converted[lower:upper])
-            this_frame = '{}'.format(self.uncover_message(covered))
-            reco_message += this_frame
-            row += len(self.uncover_message(covered))
-            if row > 130:
-                reco_message += '\n'
-                row = 0
-    
-            fig = make_fig(original[0].cpu(), covered[0].cpu(), reco_message)
-            fig.savefig('{}/{}.png'.format(save_path, idx))
-            
-    #
     def convert_message(self, message='', n_images=0):
         w_list = re.findall(r"[\w']+|[.,!?;]", message)
-        pad_length = 5*max( (len(w_list)-1)//5+1, n_images)-len(w_list)
+        pad_length = self.n_words*max( (len(w_list)-1)//self.n_words+1, n_images)-len(w_list)
         converted = []
         for word in w_list:
             converted += [self.word2idx[word]]
@@ -118,16 +99,96 @@ class encoder_decoder():
 
             cat_image = torch.cat([cover.to(device), image], dim=1)
             return cover, image, self.Hnet(cat_image)
-    #
     
+    #
     def uncover_message(self, image=None):
         with torch.set_grad_enabled(False):
-            uncovered = self.Rnet(image)
-            encripted_message = self.decoder(uncovered)
+            revealed = self.Rnet(image)
+            encripted_message = self.decoder(revealed)
             message = ''
             for word in encripted_message:
                 embedding = word.detach().cpu().numpy().reshape((50,))
                 predicted = torch.from_numpy(
                     cos_matrix_multiplication(self.vectors, embedding))
                 message += '{} '.format(self.words[predicted.argmax()])
-            return message
+            return message, revealed
+
+    #
+    def encode_message(self, message='', n_images=0, save_path='./Example'):
+        converted = self.convert_message(message=message, n_images=n_images)
+        temp_dir = './temp'
+        if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)
+        #
+        for idx, cover in enumerate(self.coverloader):
+            lower = idx*self.n_words
+            upper = (idx+1)*self.n_words
+            if upper > len(converted):
+                break
+            cover, secret, container = self.hide_message(cover, converted[lower:upper])
+            fig = save_frame(container[0], output='{}/frame{}.png'.format(temp_dir, idx))
+
+        frames_to_gif(source_dir=temp_dir,
+                      save_path='{}/message.png'.format(save_path))
+        shutil.rmtree(temp_dir)
+
+    #
+    def decode_message(self, gif='', save_path='./message.txt'):
+        temp_dir = './temp'
+        if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)
+
+        gif_to_frames(gif=gif, save_path=temp_dir)
+        
+        frameset = GIF_dataset(folder_path=temp_dir)
+        frameloader = torch.utils.data.DataLoader(frameset, batch_size=1,
+                                                  shuffle=False, num_workers=1)
+        message = ''
+        tot_frames = len(os.listdir(temp_dir))
+        for idx, frame in enumerate(frameloader):
+            if idx == tot_frames:
+                break
+            frame = frame.to(device)
+            this_frame, revealed = self.uncover_message(frame)
+            message += ' '+this_frame
+        shutil.rmtree(temp_dir)
+        text_file = open(save_path, 'w')
+        text_file.write(message)
+        text_file.close()
+        return message
+        
+    #
+    def run_benchmark(self, message='', n_images=0, save_path='./Example',
+                      max_words=0):
+        #
+        reco_message = ''
+        converted = self.convert_message(message=message,
+                                         n_images=n_images)
+        if max_words > 0:
+            converted = converted[:max_words]
+        row = 0
+        for idx, cover in enumerate(self.coverloader):
+            if idx == len(converted)//self.n_words:
+                break
+            lower = idx*self.n_words
+            upper = (idx+1)*self.n_words
+            cover, secret, container = self.hide_message(cover, converted[lower:upper])
+            this_frame, revealed = self.uncover_message(container)
+            reco_message += ' '+this_frame
+            row += len(this_frame)
+            if row > 50:
+                reco_message += '\n'
+                row = 0            
+            #
+            frame_error, frame_psnr = image_metrics(cover[0].cpu(),
+                                                    container[0].cpu())
+            if idx == 0:
+                pix_error = frame_error
+                psnr = frame_psnr
+            else:
+                pix_error = np.concatenate([pix_error, frame_error])
+                psnr = np.concatenate([psnr, frame_psnr])
+            #
+            fig = make_fig(cover[0].cpu(), container[0].cpu(), secret[0].cpu(),
+                           revealed[0].cpu(), pix_error, psnr, reco_message)
+            fig.savefig('{}/{}.png'.format(save_path, idx))
