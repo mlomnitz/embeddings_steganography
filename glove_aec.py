5506bb6906bdb98f34aadbdd64e8000726fcf6ad
Michael Lomnitz <mllomnitz@gmail.com>
2019-06-24 18:22:03 +0000

    Add check that there are sufficient images for message

--- glove_aec.py
+++ glove_aec.py
@@ -21,7 +21,7 @@ from definitions import local_path
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class encoder_decoder(): 
+class encoder_decoder():
     def __init__(self, encoder=None, decoder=None,
                  glove_path='{}/embeddings'.format(local_path),
                  dis_path='{}/weights'.format(local_path),
@@ -37,10 +37,12 @@ class encoder_decoder():
         self.Hnet = UnetGenerator(input_nc=6, output_nc=3, num_downs=7,
                                   output_function=nn.Sigmoid).to(device)
         self.Rnet = RevealNet(output_function=nn.Sigmoid).to(device)
-        self.Hnet.load_state_dict(torch.load('{}/netH.pth.tar'.format(dis_path)))
+        self.Hnet.load_state_dict(torch.load(
+            '{}/netH.pth.tar'.format(dis_path)))
         self.Hnet.eval()
         #
-        self.Rnet.load_state_dict(torch.load('{}/netR.pth.tar'.format(dis_path)))
+        self.Rnet.load_state_dict(torch.load(
+            '{}/netR.pth.tar'.format(dis_path)))
         self.Rnet.eval()
         #
         if encoder is not None:
@@ -55,7 +57,7 @@ class encoder_decoder():
         #
         #
         self.coverloader = self.init_cover(cover_dir, cover_transform)
-        
+
     #
     @staticmethod
     def init_cover(cover_dir='', cover_transform=None):
@@ -65,18 +67,19 @@ class encoder_decoder():
                 transforms.Resize(256),
                 transforms.ToTensor()
             ])
-        
-        coverset = GIF_dataset(transform=cover_transform, folder_path=cover_dir)
+
+        coverset = GIF_dataset(transform=cover_transform,
+                               folder_path=cover_dir)
         coverloader = torch.utils.data.DataLoader(coverset, batch_size=1,
                                                   shuffle=False, num_workers=1)
         return coverloader
-    
+
     #
     def label_2_embeddings(self, labels, embedding_size=50):
         embedding = torch.FloatTensor(labels.shape[0], embedding_size).zero_()
         for idx, label in enumerate(labels):
             embedding[idx] = self.vectors[label]
-            
+
         return embedding
 
     #
@@ -97,16 +100,19 @@ class encoder_decoder():
     #
     def convert_message(self, message='', n_images=0):
         w_list = re.findall(r"[\w']+|[.,!?;]", message)
-        pad_length = self.n_words*max( (len(w_list)-1)//self.n_words+1, n_images)-len(w_list)
+        assert (len(w_list)-1)//self.n_words + \
+            1 < n_images, 'Current message is longer than available images'
+        pad_length = self.n_words * \
+            max((len(w_list)-1)//self.n_words+1, n_images)-len(w_list)
         converted = []
         for word in w_list:
             converted += [self.word2idx[word]]
         if pad_length > 0:
             seq = [self.word2idx['blank']]*pad_length
             self.words[self.word2idx['blank']] = ''
-            self.random_insert_seq(converted, seq)     
+            self.random_insert_seq(converted, seq)
         return converted
-    
+
     #
     def hide_message(self, cover=None, message=None):
         with torch.set_grad_enabled(False):
@@ -115,7 +121,7 @@ class encoder_decoder():
 
             cat_image = torch.cat([cover.to(device), image], dim=1)
             return cover, image, self.Hnet(cat_image)
-    
+
     #
     def uncover_message(self, image=None):
         with torch.set_grad_enabled(False):
@@ -123,7 +129,7 @@ class encoder_decoder():
             encripted_message = self.decoder(revealed)
             message = ''
             for word in encripted_message:
-                embedding = word.detach().reshape((1,50))
+                embedding = word.detach().reshape((1, 50))
                 predicted = nn.CosineSimilarity()(self.vectors.to(device), embedding)
                 message += '{} '.format(self.words[predicted.argmax()])
             return message, revealed
@@ -142,8 +148,10 @@ class encoder_decoder():
             upper = (idx+1)*self.n_words
             if upper > len(converted):
                 break
-            cover, secret, container = self.hide_message(cover, converted[lower:upper])
-            fig = save_frame(container[0], output='{}/frame{}.png'.format(temp_dir, idx))
+            cover, secret, container = self.hide_message(
+                cover, converted[lower:upper])
+            fig = save_frame(
+                container[0], output='{}/frame{}.png'.format(temp_dir, idx))
         progress_bar.close()
         print('Saving encoded message to {}/message.png'.format(save_path))
         frames_to_gif(source_dir=temp_dir,
@@ -157,7 +165,7 @@ class encoder_decoder():
             os.makedirs(temp_dir)
 
         gif_to_frames(gif=gif, save_path=temp_dir)
-        
+
         frameset = GIF_dataset(folder_path=temp_dir)
         frameloader = torch.utils.data.DataLoader(frameset, batch_size=1,
                                                   shuffle=False, num_workers=1)
@@ -178,7 +186,7 @@ class encoder_decoder():
         text_file.write(message)
         text_file.close()
         return message
-        
+
     #
     def run_benchmark(self, message='', n_images=0, save_path='./Example',
                       max_words=0):
@@ -194,13 +202,14 @@ class encoder_decoder():
                 break
             lower = idx*self.n_words
             upper = (idx+1)*self.n_words
-            cover, secret, container = self.hide_message(cover, converted[lower:upper])
+            cover, secret, container = self.hide_message(
+                cover, converted[lower:upper])
             this_frame, revealed = self.uncover_message(container)
             reco_message += ' '+this_frame
             row += len(this_frame)
             if row > 50:
                 reco_message += '\n'
-                row = 0            
+                row = 0
             #
             frame_error, frame_psnr = image_metrics(cover[0].cpu(),
                                                     container[0].cpu())
